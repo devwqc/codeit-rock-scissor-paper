7146f5d71f0aef25f07f6181fe305ca16c491c7e
devwqc <devwqc@gmail.com>
2024-02-03 14:03:45 +0900

    refactor: Change from class to className in JSX

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <h1 id="title">가위바위보</h1>
-    <button class="hand">가위</button>
-    <button class="hand">바위</button>
-    <button class="hand">보</button>
+    <button className="hand">가위</button>
+    <button className="hand">바위</button>
+    <button className="hand">보</button>
   </>
 );
