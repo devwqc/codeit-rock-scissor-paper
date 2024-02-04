e5625a50001e58d8acef3c7c78f5e56912ddac82
devwqc <devwqc@gmail.com>
2024-02-04 23:31:03 +0900

    feat: Create HandIcon component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <h1 id="title">가위바위보</h1>
-    <button className="hand">가위</button>
-    <button className="hand">바위</button>
-    <button className="hand">보</button>
-  </>
-);
+root.render(<App />);
