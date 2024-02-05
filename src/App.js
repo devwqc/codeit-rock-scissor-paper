d46fd33e920497e29b3f98de18a3691ab5687f10
devwqc <devwqc@gmail.com>
2024-02-05 17:31:21 +0900

    feat: 승부 기록 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HandButton from './HandButton';
 import HandIcon from './HandIcon';
 import { compareHand, generateRandomHand } from './utils';
 
+const INITIAL_VALUE = 'rock';
+
 function getResult(me, other) {
   const comparison = compareHand(me, other);
   if (comparison > 0) return '승리';
@@ -12,28 +14,33 @@ function getResult(me, other) {
 }
 
 function App() {
-  const [hand, setHand] = useState('rock');
-  const [otherHand, setOtherHand] = useState('scissor');
+  const [hand, setHand] = useState(INITIAL_VALUE);
+  const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
+  const [gameHistory, setGameHistory] = useState([]);
 
   const handleButtonClick = (nextHand) => {
+    const nextOtherHand = generateRandomHand();
+    const nextHistoryItem = getResult(nextHand, nextOtherHand);
     setHand(nextHand);
-    setOtherHand(generateRandomHand());
+    setOtherHand(nextOtherHand);
+    setGameHistory([...gameHistory, nextHistoryItem]);
   };
 
   const handleClearClick = () => {
-    setHand('rock');
-    setOtherHand('rock');
+    setHand(INITIAL_VALUE);
+    setOtherHand(INITIAL_VALUE);
+    setGameHistory([]);
   };
 
   return (
     <div>
       <Button onClick={handleClearClick}>처음부터</Button>
-      <p>{getResult(hand, otherHand)}</p>
       <div>
         <HandIcon value={hand} />
         VS
         <HandIcon value={otherHand} />
       </div>
+      <p>승부 기록: {gameHistory.join(', ')}</p>
       <div>
         <HandButton value="rock" onClick={handleButtonClick} />
         <HandButton value="scissor" onClick={handleButtonClick} />
